e210b91d3be3c444bbcd7d192e0058ed9990d4cd
TomaszTokarz <tomektokarz@tlen.pl>
2017-09-19 21:05:57 +0200

    first user events

--- src/components/login.js
+++ src/components/login.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Login extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+    this.ui = {
+      loginInput : '.js-login-input'
+    }
+  }
+
+  showPasswordField(event) {
+    console.log(event.target);
+    console.log(this.ui);
+  };
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
-      <div>
-        <div className="login__title">Log in</div>
-        <Link className="button button__login" to="/password">
-          <span>Log in</span>
-        </Link>
-        <div className="login__title">Sign in</div>
-        <Link className="button button__login" to="/signin">
-          <span>Sign in</span>
-        </Link>
+      <div className="login">
+        <input type="email" onChange={event => this.setState({ term: event.target.value })} className="login__input js-login-input" placeholder="Type your e-mail"></input>
+        <input type="password" className="login__input inactive"></input>
+        <button className="login__button" onClick={this.showPasswordField}>Log in</button>
+
+        <input type="email" className="login__input" placeholder="Type your e-mail"></input>
+        <button className="login__button">Sign in</button>
       </div>
     );
   }
