7d8e8329b89c9abcdf836241705e5955fa11fc9e
TomaszTokarz <tomektokarz@tlen.pl>
2017-11-01 18:22:42 +0100

    redux forms

--- src/components/login.js
+++ src/components/login.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Field, reduxForm } from "redux-form";
 
-export default class Login extends Component {
+class Login extends Component {
+  renderEmailField(field) {
+    return (
+      <div>
+
+        <input
+          className="login__input"
+          placeholder="Type your e-mail"
+          type="email"
+          {...field.input}
+        />
+      </div>
+    );
+  }
 
   constructor(props) {
     super(props);
 
-    this.state = { term: ''};
+    this.state = { term: ""};
     this.ui = {
-      loginInput : '.js-login-input'
+      loginInput : ".js-login-input"
     }
   }
 
   showPasswordField(event) {
     console.log(event.target);
     console.log(this.ui);
-  };
+  }
 
   onInputChange(event) {
     console.log(event.target.value);
@@ -24,13 +40,23 @@ export default class Login extends Component {
   render() {
     return (
       <div className="login">
-        <input type="email" onChange={event => this.setState({ term: event.target.value })} className="login__input js-login-input" placeholder="Type your e-mail"></input>
-        <input type="password" className="login__input inactive"></input>
-        <button className="login__button" onClick={this.showPasswordField}>Log in</button>
+        <form>
+          <Field
+            name="email"
+            component={this.renderEmailField}
+          />
 
-        <input type="email" className="login__input" placeholder="Type your e-mail"></input>
-        <button className="login__button">Sign in</button>
+          <input type="password" className="login__input inactive"></input>
+          <button className="login__button" onClick={this.showPasswordField}>Log in</button>
+
+          <input type="email" className="login__input" placeholder="Type your e-mail"></input>
+          <button className="login__button">Sign in</button>
+        </form>
       </div>
     );
   }
 }
+
+export default reduxForm({
+  form: "LoginForm"
+})(Login);
